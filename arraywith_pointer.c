23695adb085c97805e947005c9d7fa080ee5b953
SAMIUL2022 <sami35bd@gmail.com>
2023-10-28 18:33:43 +0600

    added array with function

---
+++ arraywith_pointer.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+int ar[5] ={10,20,30,40,50};
+
+printf("%p\n",&ar[0]);
+printf("%p",ar);
+
+    return 0;
+}
---
+++ arraywithfunction.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+void fun (int *ab,int n)
+{
+for (int i = 0; i < n; i++)
+{
+   printf("%d ",ab[i]);
+}
+
+
+
+
+}
+int main ()
+{
+int n;
+scanf("%d",&n);
+int ar[n];
+for (int i = 0; i < n; i++)
+{
+  scanf("%d",&ar[i]);
+}
+
+fun(ar,n);
+
+    return 0;
+}
